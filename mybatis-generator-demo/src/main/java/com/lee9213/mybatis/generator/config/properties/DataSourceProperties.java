31cca459489b6b7c8ec47e136ad78832b1ba604a
libo <libo02@jumapeisong.com>
2018-10-22 15:10:42 +0800

    优化配置

--- mybatis-generator-demo/src/main/java/com/lee9213/mybatis/generator/config/properties/DataSourceProperties.java
+++ mybatis-generator-demo/src/main/java/com/lee9213/mybatis/generator/config/properties/DataSourceProperties.java
@@ -1,17 +1,13 @@
 package com.lee9213.mybatis.generator.config.properties;
 
-import com.lee9213.mybatis.generator.config.converts.ITypeConvert;
-import com.lee9213.mybatis.generator.config.converts.MySqlTypeConvert;
-import com.lee9213.mybatis.generator.config.domain.DbType;
+import com.lee9213.mybatis.generator.config.sql.converts.ITypeConvert;
+import com.lee9213.mybatis.generator.config.sql.converts.MySqlTypeConvert;
+import com.lee9213.mybatis.generator.config.sql.enums.DbType;
 import com.lee9213.mybatis.generator.config.sql.query.IDbQuery;
 import com.lee9213.mybatis.generator.config.sql.query.MySqlQuery;
 import lombok.Data;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 /**
  * <p>数据库配置信息</p>
@@ -28,7 +24,7 @@ public class DataSourceProperties {
     /**
      * 数据库信息查询
      */
-    private IDbQuery dbQuery;
+    private IDbQuery dbQuery = new MySqlQuery();
     /**
      * 数据库类型
      */
@@ -40,7 +36,7 @@ public class DataSourceProperties {
     /**
      * 类型转换
      */
-    private ITypeConvert typeConvert;
+    private ITypeConvert typeConvert = new MySqlTypeConvert();
     /**
      * 驱动连接的URL
      */
@@ -57,61 +53,4 @@ public class DataSourceProperties {
      * 数据库连接密码
      */
     private String password = "123456";
-
-    public IDbQuery getDbQuery() {
-        if (null == dbQuery) {
-            switch (getDbType()) {
-                default:
-                    // 默认 MYSQL
-                    dbQuery = new MySqlQuery();
-                    break;
-            }
-        }
-        return dbQuery;
-    }
-
-    public ITypeConvert getTypeConvert() {
-        if (null == typeConvert) {
-            switch (getDbType()) {
-                case MARIADB:
-                    typeConvert = new MySqlTypeConvert();
-                    break;
-                default:
-                    // 默认 MYSQL
-                    typeConvert = new MySqlTypeConvert();
-                    break;
-            }
-        }
-        return typeConvert;
-    }
-
-    /**
-     * 判断数据库类型
-     *
-     * @return 类型枚举值
-     */
-    public DbType getDbType() {
-        if (null == dbType) {
-            if (driverName.contains("mysql")) {
-                dbType = DbType.MYSQL;
-            }
-        }
-        return dbType;
-    }
-
-    /**
-     * 创建数据库连接对象
-     *
-     * @return Connection
-     */
-    public Connection getConn() {
-        Connection conn = null;
-        try {
-            Class.forName(driverName);
-            conn = DriverManager.getConnection(url, username, password);
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        }
-        return conn;
-    }
 }
