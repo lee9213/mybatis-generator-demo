9b4c1a6679a953dc7a76f1c2d25c050ee3de8b46
libo <libo02@jumapeisong.com>
2018-10-22 17:23:38 +0800

    优化配置

--- mybatis-generator-demo/src/main/java/com/lee9213/mybatis/generator/config/properties/StrategyProperties.java
+++ mybatis-generator-demo/src/main/java/com/lee9213/mybatis/generator/config/properties/StrategyProperties.java
@@ -1,9 +1,15 @@
 package com.lee9213.mybatis.generator.config.properties;
 
+import com.lee9213.mybatis.generator.util.CollectionUtils;
 import com.lee9213.mybatis.generator.util.Constant;
 import com.lee9213.mybatis.generator.util.StringUtils;
+import lombok.Data;
+import lombok.experimental.Accessors;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * <p>策略配置信息</p>
@@ -14,17 +20,33 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  */
 @SpringBootConfiguration
 @ConfigurationProperties(prefix = "strategy")
+@Data
+@Accessors(chain = true)
 public class StrategyProperties {
 
     /**
-     * 是否大写命名
+     * 表前缀
      */
-    private boolean isCapitalMode = false;
+    private List<String> tablePrefix;
 
     /**
-     * 是否跳过视图
+     * 字段前缀
      */
-    private boolean skipView = false;
+    private List<String> fieldPrefix;
+
+    /**
+     * 需要包含的表名
+     */
+    private List<String> includeTables = null;
+    /**
+     * 需要包含的表名的前缀
+     */
+    private List<String> includeTablePrefixs = null;
+
+    /**
+     * 需要排除的表名
+     */
+    private List<String> excludeTables = null;
 
     /**
      * 数据库表映射到实体的命名策略，是否转换成驼峰
@@ -36,16 +58,6 @@ public class StrategyProperties {
     private String underlineToCamelColumnNames = "true";
 
     /**
-     * 表前缀
-     */
-    private String[] tablePrefix;
-
-    /**
-     * 字段前缀
-     */
-    private String[] fieldPrefix;
-
-    /**
      * 自定义继承的Entity类全称，带包名
      */
     private String superEntityClass;
@@ -53,7 +65,7 @@ public class StrategyProperties {
     /**
      * 自定义基础的Entity类，公共字段
      */
-    private String[] superEntityColumns;
+    private List<String> superEntityColumns;
 
     /**
      * 自定义继承的Mapper类全称，带包名
@@ -76,359 +88,37 @@ public class StrategyProperties {
     private String superControllerClass;
 
     /**
-     * 需要包含的表名
-     */
-    private String[] includeTables = null;
-    /**
-     * 需要包含的表名的前缀
-     */
-    private String[] includeTablePrefixs = null;
-
-    /**
-     * 需要排除的表名
-     */
-    private String[] excludeTables = null;
-    /**
-     * 【实体】是否生成字段常量（默认 false）<br>
+     * 【实体】setter方法是否为构建者模型（默认 false）<br>
      * -----------------------------------<br>
-     * public static final String ID = "test_id";
-     */
-    private boolean entityColumnConstant = false;
-
-    /**
-     * 【实体】是否为构建者模型（默认 false）<br>
-     * -----------------------------------<br>
-     * public User setName(String name) { this.name = name; return this; }
+     * for example : public User setName(String name) { this.name = name; return this; }
      */
     private boolean entityBuilderModel = false;
 
     /**
-     * 【实体】是否为lombok模型（默认 false）<br>
-     * <a href="https://projectlombok.org/">document</a>
+     * 【实体】是否为lombok模型（默认 false）<a href="https://projectlombok.org/">document</a>
      */
     private boolean entityLombokModel = false;
 
     /**
-     * Boolean类型字段是否移除is前缀（默认 false）<br>
-     * 比如 : 数据库字段名称 : 'is_xxx',类型为 : tinyint. 在映射实体的时候则会去掉is,在实体类中映射最终结果为 xxx
-     */
-    private boolean entityBooleanColumnRemoveIsPrefix = false;
-    /**
      * 生成 <code>@RestController</code> 控制器
      * <pre>
-     *      <code>@Controller</code> -> <code>@RestController</code>
+     *  <code>@Controller</code> -> <code>@RestController</code>
      * </pre>
      */
     private boolean restControllerStyle = false;
-    /**
-     * 驼峰转连字符
-     * <pre>
-     *      <code>@RequestMapping("/managerUserActionHistory")</code> -> <code>@RequestMapping("/manager-user-action-history")</code>
-     * </pre>
-     */
-    private boolean controllerMappingHyphenStyle = false;
-
-    /**
-     * 是否生成实体时，生成字段注解
-     */
-    private boolean entityTableFieldAnnotationEnable = false;
-    /**
-     * 乐观锁属性名称
-     */
-    private String versionFieldName;
 
     /**
      * 逻辑删除属性名称
      */
     private String logicDeleteFieldName;
-//
-//    /**
-//     * 表填充字段
-//     */
-//    private List<TableFill> tableFillList = null;
-
-    /**
-     * <p>
-     * 大写命名、字段符合大写字母数字下划线命名
-     * </p>
-     *
-     * @param word 待判断字符串
-     * @return
-     */
-    public boolean isCapitalModeNaming(String word) {
-        return isCapitalMode && StringUtils.isCapitalMode(word);
-    }
-
-    /**
-     * <p>
-     * 表名称包含指定前缀
-     * </p>
-     *
-     * @param tableName 表名称
-     * @return
-     */
-    public boolean containsTablePrefix(String tableName) {
-        if (null != tableName) {
-            String[] tps = getTablePrefix();
-            if (null != tps) {
-                for (String tp : tps) {
-                    if (tableName.contains(tp)) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    public boolean isCapitalMode() {
-        return isCapitalMode;
-    }
-
-    public StrategyProperties setCapitalMode(boolean isCapitalMode) {
-        this.isCapitalMode = isCapitalMode;
-        return this;
-    }
-
-    public boolean isSkipView() {
-        return skipView;
-    }
-
-    public StrategyProperties setSkipView(boolean skipView) {
-        this.skipView = skipView;
-        return this;
-    }
-
-    public String getUnderlineToCamelTableName() {
-        return underlineToCamelTableName;
-    }
-
-    public void setUnderlineToCamelTableName(String underlineToCamelTableName) {
-        this.underlineToCamelTableName = underlineToCamelTableName;
-    }
-
-    public String getUnderlineToCamelColumnNames() {
-        return underlineToCamelColumnNames;
-    }
-
-    public void setUnderlineToCamelColumnNames(String underlineToCamelColumnNames) {
-        this.underlineToCamelColumnNames = underlineToCamelColumnNames;
-    }
-
-    public String[] getTablePrefix() {
-        return tablePrefix;
-    }
-
-    public StrategyProperties setTablePrefix(String... tablePrefix) {
-        this.tablePrefix = tablePrefix;
-        return this;
-    }
-
-    public String getSuperEntityClass() {
-        return superEntityClass;
-    }
-
-    public StrategyProperties setSuperEntityClass(String superEntityClass) {
-        this.superEntityClass = superEntityClass;
-        return this;
-    }
 
     public boolean includeSuperEntityColumns(String fieldName) {
-        if (null != superEntityColumns) {
-            for (String column : superEntityColumns) {
-                if (column.equals(fieldName)) {
-                    return true;
-                }
-            }
+        if(StringUtils.isNotEmpty(fieldName) && CollectionUtils.isNotEmpty(superEntityColumns)) {
+            return superEntityColumns.stream()
+                    .map(key -> key.toLowerCase())
+                    .collect(Collectors.toList())
+                    .contains(fieldName.toLowerCase());
         }
         return false;
-    }
-
-    public String[] getSuperEntityColumns() {
-        return superEntityColumns;
-    }
-
-    public StrategyProperties setSuperEntityColumns(String... superEntityColumns) {
-        this.superEntityColumns = superEntityColumns;
-        return this;
-    }
-
-    public String getSuperMapperClass() {
-        return superMapperClass;
-    }
-
-    public StrategyProperties setSuperMapperClass(String superMapperClass) {
-        this.superMapperClass = superMapperClass;
-        return this;
-    }
-
-    public String getSuperServiceClass() {
-        return superServiceClass;
-    }
-
-    public StrategyProperties setSuperServiceClass(String superServiceClass) {
-        this.superServiceClass = superServiceClass;
-        return this;
-    }
-
-    public String getSuperServiceImplClass() {
-        return superServiceImplClass;
-    }
-
-    public StrategyProperties setSuperServiceImplClass(String superServiceImplClass) {
-        this.superServiceImplClass = superServiceImplClass;
-        return this;
-    }
-
-    public String getSuperControllerClass() {
-        return superControllerClass;
-    }
-
-    public StrategyProperties setSuperControllerClass(String superControllerClass) {
-        this.superControllerClass = superControllerClass;
-        return this;
-    }
-
-    public String[] getIncludeTables() {
-        return includeTables;
-    }
-
-    public StrategyProperties setIncludeTables(String... includeTables) {
-        this.includeTables = includeTables;
-        return this;
-    }
-
-    public String[] getIncludeTablePrefixs() {
-        return includeTablePrefixs;
-    }
-
-    public StrategyProperties setIncludeTablePrefixs(String... includeTablePrefixs) {
-        this.includeTablePrefixs = includeTablePrefixs;
-        return this;
-    }
-
-    public String[] getExcludeTables() {
-        return excludeTables;
-    }
-
-    public StrategyProperties setExcludeTables(String... excludeTables) {
-        this.excludeTables = excludeTables;
-        return this;
-    }
-
-    public StrategyProperties setExcludeTabless(String[] excludeTables) {
-        this.excludeTables = excludeTables;
-        return this;
-    }
-
-    public boolean isEntityColumnConstant() {
-        return entityColumnConstant;
-    }
-
-    public StrategyProperties setEntityColumnConstant(boolean entityColumnConstant) {
-        this.entityColumnConstant = entityColumnConstant;
-        return this;
-    }
-
-    public boolean isEntityBuilderModel() {
-        return entityBuilderModel;
-    }
-
-    public StrategyProperties setEntityBuilderModel(boolean entityBuilderModel) {
-        this.entityBuilderModel = entityBuilderModel;
-        return this;
-    }
-
-    public boolean isEntityLombokModel() {
-        return entityLombokModel;
-    }
-
-    public StrategyProperties setEntityLombokModel(boolean entityLombokModel) {
-        this.entityLombokModel = entityLombokModel;
-        return this;
-    }
-
-    public boolean isEntityBooleanColumnRemoveIsPrefix() {
-        return entityBooleanColumnRemoveIsPrefix;
-    }
-
-    public StrategyProperties setEntityBooleanColumnRemoveIsPrefix(boolean entityBooleanColumnRemoveIsPrefix) {
-        this.entityBooleanColumnRemoveIsPrefix = entityBooleanColumnRemoveIsPrefix;
-        return this;
-    }
-
-    public boolean isRestControllerStyle() {
-        return restControllerStyle;
-    }
-
-    public StrategyProperties setRestControllerStyle(boolean restControllerStyle) {
-        this.restControllerStyle = restControllerStyle;
-        return this;
-    }
-
-    public boolean isControllerMappingHyphenStyle() {
-        return controllerMappingHyphenStyle;
-    }
-
-    public StrategyProperties setControllerMappingHyphenStyle(boolean controllerMappingHyphenStyle) {
-        this.controllerMappingHyphenStyle = controllerMappingHyphenStyle;
-        return this;
-    }
-
-    public String getLogicDeleteFieldName() {
-        return logicDeleteFieldName;
-    }
-
-    /**
-     * 设置逻辑删除字段
-     *
-     * @param logicDeleteFieldName 数据库字段
-     * @return
-     */
-    public StrategyProperties setLogicDeleteFieldName(String logicDeleteFieldName) {
-        this.logicDeleteFieldName = logicDeleteFieldName;
-        return this;
-    }
-
-    public String getVersionFieldName() {
-        return versionFieldName;
-    }
-
-    /**
-     * 设置乐观锁字段
-     *
-     * @param versionFieldName 数据库字段
-     * @return
-     */
-    public StrategyProperties setVersionFieldName(String versionFieldName) {
-        this.versionFieldName = versionFieldName;
-        return this;
-    }
-
-//    public List<TableFill> getTableFillList() {
-//        return tableFillList;
-//    }
-//
-//    public StrategyConfiguration setTableFillList(List<TableFill> tableFillList) {
-//        this.tableFillList = tableFillList;
-//        return this;
-//    }
-
-    public String[] getFieldPrefix() {
-        return fieldPrefix;
-    }
-
-    public StrategyProperties setFieldPrefix(String... fieldPrefixs) {
-        this.fieldPrefix = fieldPrefixs;
-        return this;
-    }
-
-    public StrategyProperties entityTableFieldAnnotationEnable(boolean isEnableAnnotation) {
-        entityTableFieldAnnotationEnable = isEnableAnnotation;
-        return this;
-    }
-
-    public boolean isEntityTableFieldAnnotationEnable() {
-        return entityTableFieldAnnotationEnable;
     }
 }
