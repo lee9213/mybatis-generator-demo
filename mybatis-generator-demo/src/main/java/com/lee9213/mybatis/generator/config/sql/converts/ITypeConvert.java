66af259341569f60bc2b64d6f1c1b707018746c1
libo <libo02@jumapeisong.com>
2018-10-23 11:19:30 +0800

    优化配置

--- mybatis-generator-demo/src/main/java/com/lee9213/mybatis/generator/config/sql/converts/ITypeConvert.java
+++ mybatis-generator-demo/src/main/java/com/lee9213/mybatis/generator/config/sql/converts/ITypeConvert.java
@@ -4,6 +4,8 @@ import com.lee9213.mybatis.generator.config.properties.GlobalProperties;
 import com.lee9213.mybatis.generator.config.sql.rules.IColumnType;
 
 /**
+ * <p>类型转换</p>
+ *
  * @author lee9213@163.com
  * @date 2018/10/18 17:19
  */
